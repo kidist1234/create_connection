4a0de0e98ec754b476aec4b6237e7203ce57eea6
kidist <kidist2002@gmail.com>
2024-10-03 11:45:28 +0300

    merge

--- start.cpp
+++ start.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
+#include <string>
 int main(){
-    std::cout<<"Hello World\n"<<std::endl;
+  /* int myAge;
+    std::string fullName;
+    std::string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::cout << "The length of the txt string is: " << txt.length()<<"\n";
+    std::cout << "The length of the txt string is: " << txt.size()<<"\n";
+    std::cout<<"Hello World1\n";
+    std::cout << "I am learning C++\n";
+   // std::cin>>myAge;
+  //  int myAge=myAge//;
+   // std::cout << "I am " << myAge << " years old.";
+    std::cout<<"enter you fullname\n";
+    //std::cin>>fullName;
+    //std::getline(cin, fullname);
+   // getline (cin, fullName);
+    std::getline (std::cin, fullName);
+    std::cout<<fullName;
+    */
+   int num;
+   std::cout<<"enter a number";
+   std::cin>>num;
+   for(int i = 1; i<=10;i++)
+   {
+    std::cout<<num <<"*" <<i<<"= "<<num*i<<"\n";
+   }
     return 0;
 }
